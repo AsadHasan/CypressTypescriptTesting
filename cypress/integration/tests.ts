c67a2e46e957ffffa2dbacf29b4c90866461a14d
Asad Hasan <asadhasan180@hotmail.com>
2019-04-03 12:26:07 +0100

    completed and updated test

--- cypress/integration/tests.ts
+++ cypress/integration/tests.ts
@@ -5,6 +5,19 @@ describe("On the homepage", () => {
   it("Tech category should return tech & coding courses", () => {
     const homepage: Homepage = new Homepage();
     const searchResults: SearchResults = homepage.GetTechCourses();
-    searchResults.GetResultsCategories();
+
+    const actualResults = searchResults.GetResultsCategories();
+    const expectedResults: string[] = [
+      "Business Technology",
+      "Cyber Security",
+      "Data Science",
+      "Digital Marketing"
+    ];
+
+    actualResults
+      .should("have.length", expectedResults.length)
+      .each(($actualResult, index) =>
+        expect($actualResult.text()).to.be.equal(expectedResults[index])
+      );
   });
 });
